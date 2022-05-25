490ce0e61e541ffebe84c0f8c77f2ad2106969c1
LuizH64 <60182096+LuizH64@users.noreply.github.com>
2022-05-25 13:55:12 -0300

    Arrumando funções do domManipulator

--- scripts/utils/domManipulator.js
+++ scripts/utils/domManipulator.js
@@ -1,10 +1,7 @@
-export const addClass = (domElement, className) => {
-    domElement.classList.add(className);
-}
+export const addClass = (domElement, className) => domElement.classList.add(className);
 
-export const removeClass = (domElement, className) => {
-    domElement.classList.remove(className);
-}
+export const removeClass = (domElement, className) => domElement.classList.remove(className);
+
 
 export const append = (domElement, child) => {
     if (typeof child === 'string') child = document.createTextNode(child);
