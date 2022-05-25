35cad09996fc0775791f6d975f8cf9805d7ec47d
Luiz Rodrigues <luizhenriquealvesrodrigues64@gmail.com>
2022-05-25 20:39:24 -0300

    Ordenando por popularidade

--- scripts/requests/requests.js
+++ scripts/requests/requests.js
@@ -1,6 +1,6 @@
 import makeRequest from "../utils/requestHandler.js";
 
 export const fetchGamesList = async () => {
-    const games = await makeRequest('games');
+    const games = await makeRequest('games?sort-by=popularity');
     return games;
 }
